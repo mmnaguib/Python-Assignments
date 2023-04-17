47dc56aae16218995fcb2cc08031dbaf576f024f
Mohamed Naguib <47403212+mmnaguib@users.noreply.github.com>
2023-04-17 15:06:05 +0200

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,255 @@
+#------- assignments 01 To 10 Videos ----------
+    #------- assign 1 ------- 
+        # This File created by Naguib
+        # This File created at 12/4/2023
+        # This File Will Contain The Anwsers for Assignments
+    #------- assign 1 ------- 
+
+    #------- assign 2, assign 3, assign 4, assign 5 ------- 
+        #name = 'Mohamed'
+        #age = '36'
+        #country = 'Egypt'
+        #print('\"Name:' + name + '\"')
+        #print('\"Age:' + age + '\"')
+        #print('\"Country:' + country + '\"')
+        #print('\"Hello, My Name is' + name + 'And Iam' + age + 'Years Old and I Live in Country:' + country + '\"')
+        #print(type(name));print(type(age));print(type(country));
+    #------- assign 2, assign 3, assign 4, assign 5 ------- 
+#------- assignments 01 To 10 Videos ----------
+
+#------- assignments 11 To 18 Videos ----------
+    #------- assign 1, assign 2 -------
+        #name = 'Mohamed'
+        #age = '24'
+        #country = 'Egypt'
+
+        #print('\"Hello \'' + name + '\', How You Doing \ \"\"\" Your Age Is \"'+ age +'\"\" And Your Country Is:' + country)
+        #print(""""Hello '""" + name +  """', How You Doing \\
+        #\"\"\" Your Age Is \""""+ age+ """\"" +
+        #And Your Country Is:""" + country )
+    #------- assign 1, assign 2 ------- 
+
+    #------- assign 3 , assign 4 -------
+        #name = 'Elzero'
+        #print('Second Letter Is "' + name[1] + '"')
+        #print('Third Letter Is "' + name[2] + '"')
+        #print('Last Letter Is "' + name[-1] + '"')
+        #print(name[1:4])
+        #print(name[::2])
+        #print(name[-2::-2])
+    #------- assign 3 , assign 4 ------- 
+
+    #------- assign 5 ------- 
+        #name = '#@#@Elzero#@#@'
+        #print(name.strip("@#"))
+    #------- assign 5 ------- 
+
+    #------- assign 6 ------- 
+        #a = "9"
+        #b = "15"
+        #c = "130"
+        #d = "950"
+        #e = "1500"
+        #print(a.zfill(4));print(b.zfill(4));print(c.zfill(4));print(d.zfill(4));print(e.zfill(4))
+    #------- assign 6 ------- 
+
+    #------- assign 7 ------- 
+        #name_one = "Osama"
+        #name_two = "Osama_Elzero"
+        #print(name_one.rjust(20,'@'));
+        #print(name_two.rjust(20,'@'));
+    #------- assign 7 ------- 
+
+    #------- assign 8 ------- 
+        #name_one = "OSamA"
+        #name_two = "osaMA"
+        #print(name_one.swapcase());
+        #print(name_two.swapcase());
+    #------- assign 8 -------
+
+    #------- assign 9 ------- 
+        #msg = "I Love Python And Although Love Elzero Web School"
+        #print(msg.count('Love'))
+    #------- assign 9 ------- 
+
+    #------- assign 10 ------- 
+        #name = "Elzero"
+        #print(name.index('z'))
+        #------- assign 10 ------- 
+
+    #------- assign 11, assign 12 ------- 
+        #msg = "I <3 Python And Although <3 Elzero Web School"
+        #print(msg.replace('<3','Love',1))
+        #print(msg.replace('<3','Love'))
+    #------- assign 11, assign 12 ------- 
+
+    #------- assign 13 ------- 
+        #name = "Osama"
+        #age = 38
+        #country = "Egypt"
+        #print('My Name Is {:s}, And My Age Is {:d}, And My Country Is {:s}' .format(name,age,country))
+        #print(f'My Name Is {name}, And My Age Is {age}, And My Country Is {country}')
+    #------- assign 13 ------- 
+#------- assignments 11 To 18 Videos ----------
+
+#------- assignments 19 To 20 Videos ----------
+    #------- assign 1 ------- 
+        #print(10);print(-10);print(10.1);print(-1.1);print(10+5j);
+    #------- assign 1 ------- 
+
+    #------- assign 2 ------- 
+        #number = 1+2j
+        #print(number.real)
+        #print(number.imag)
+    #------- assign 2 ------- 
+
+    #------- assign 3 ------- 
+        #num = 10
+        #print('{:.10f}'.format(float(num)))
+    #------- assign 3 ------- 
+
+    #------- assign 4 ------- 
+        #num = 159.650
+        #print(int(num))
+        #print(type(int(num)))
+    #------- assign 4 ------- 
+
+    #------- assign 5 ------- 
+        #print(100 - 115) #= -15
+        #print(50 * 30) #= 1500
+        #print(21 % 4) #= 1
+        #print(int(110 / 11)) #= 10
+        #print(97 // 20) #= 4
+    #------- assign 5 ------- 
+#------- assignments 19 To 20 Videos ----------
+
+#------- assignments 21 To 23 Videos ----------
+    #------- assign 1 -------
+        #friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+        #print(friends[0]);print(friends[0:1][0]);
+        #print(friends[-1]);print(friends[-1:][0]);
+    #------- assign 1 -------
+
+    #------- assign 2 -------
+        #friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+        #print(friends[::2]);print(friends[1::2]);
+    #------- assign 2 -------
+
+    #------- assign 3 -------
+        #friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+        #print(friends[1:4]);
+        #print(friends[-2:]);
+    #------- assign 3 -------
+
+    #------- assign 4 -------
+        #friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+        #friends[-2:] = ['Elzero','elzero']
+        #print(friends)
+    #------- assign 4 -------
+
+    #------- assign 5 -------
+        #friends = ["Osama", "Ahmed", "Sayed"]
+        #friends.insert(0,'Nasser')
+        #print(friends)
+        #friends.append('Salem')
+        #print(friends)
+    #------- assign 5 -------
+
+    #------- assign 6 -------
+        #friends = ["Nasser", "Osama", "Ahmed", "Sayed", "Salem"]
+        #friends[0:2] = []
+        #print(friends)
+        #friends.remove(friends[-1])
+        #print(friends)
+    #------- assign 6 -------
+
+    #------- assign 7 -------
+        #friends = ["Ahmed", "Sayed"]
+        #employees = ["Samah", "Eman"]
+        #school = ["Ramy", "Shady"]
+        #friends.extend(employees)
+        #friends.extend(school)
+        #print(friends)
+    #------- assign 7 -------
+
+    #------- assign 8 -------
+        #friends = ["Ahmed", "Sayed", "Samah", "Eman", "Ramy", "Shady"]
+        #friends.sort()
+        #print(friends)
+        #friends.sort(reverse=True)
+        #print(friends)
+    #------- assign 8 -------
+
+    #------- assign 9 -------
+        #friends = ["Ahmed", "Sayed", "Samah", "Eman", "Ramy", "Shady"]
+        #print(len(friends))
+    #------- assign 9 -------
+
+    #------- assign 10 -------
+        #technologies = ["Html", "CSS", "JS", "Python", ["Django", "Flask", "Web","Django2", "Flask2", "Web2"]]
+        #print(technologies[-1][0])
+        #print(technologies[-1][-1])
+    #------- assign 10 -------
+#------- assignments 21 To 23 Videos ----------
+
+#------- assignments 24 To 25 Videos ----------
+    #------- assign 1 -------
+        #tupleName = "Mohamed",
+        #print(tupleName[0])
+        #print(type(tupleName))
+    #------- assign 1 -------
+
+    #------- assign 2 -------
+        #friends = ("Osama", "Ahmed", "Sayed")
+        #friends = list(friends)
+        #friends[0]= "elzero"
+        #friends = tuple(friends)
+        #print(friends);
+        #print(type(friends))
+        #print(len(friends))
+    #------- assign 2 -------
+
+    #------- assign 3 -------
+        #nums = (1, 2, 3)
+        #letters = ("A", "B", "C")
+        #nums_and_letters_one = nums + letters
+        #print(nums_and_letters_one);print(len(nums_and_letters_one))
+    #------- assign 3 -------
+
+    #------- assign 4 -------
+        #my_tuple = (1, 2, 3, 4)
+        #a,b,_,c = my_tuple
+        #print(a)
+        #print(b)
+        #print(c)
+    #------- assign 4 -------
+#------- assignments 24 To 25 Videos ----------
+
+#------- assignments 26 To 32 Videos ----------
+    #------- assign 1 -------
+        #my_list = [1, 2, 3, 3, 4, 5, 1]
+        #my_unique = set(my_list);
+        #my_unique = list(my_unique)
+        #print(my_unique);
+        #print(type(my_unique));
+        #my_unique.remove(my_unique[-1])
+        #print(my_unique)
+    #------- assign 1 -------
+
+    #------- assign 2 -------
+nums = {1, 2, 3}
+letters = {"A", "B", "C"}
+print(nums | letters)
+print(nums.union(letters))
+print(nums.update(letters))
+    #------- assign 2 -------
+
+    #------- assign 3 -------
+    #------- assign 3 -------
+
+    #------- assign 4 -------
+    #------- assign 4 -------
+
+    #------- assign 5 -------
+    #------- assign 5 -------
+#------- assignments 26 To 32 Videos ----------
